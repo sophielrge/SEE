9a8ac2047883bfb85aacb07ae2f7933479ae7367
Elise Gimond <elisegimond@gmail.com>
2024-10-07 16:21:01 +0200

    tables

--- SEE/sql/Tables.sql
+++ SEE/sql/Tables.sql
@@ -1,6 +1,28 @@
-CREATE TABLE User (
+CREATE TABLE TApplicant (
     id INT PRIMARY KEY,
     nom VARCHAR(100),
-    email VARCHAR(100),
+    age INT,
+    dpt INT,
+);
+
+CREATE TABLE TVolunteer(
+    id INT PRIMARY KEY,
+    nom VARCHAR(100),
+    age INT,
+    dpt INT,
+
+);
+
+CREATE TYPE statut AS ENUM ('pending', 'approved', 'rejected','completed');
+
+CREATE TABLE TRequest (
+    id INT PRIMARY KEY,
+    subj VARCHAR(100),
+    id_volunteer INT, --doit lier un Volunteer
+    id_applicant INT, --doit lier un Applicant
+    statut statut, --doit lier un type statut
+    helpday DATE,
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CONSTRAINT fk_volunteer FOREIGN KEY (id_volunteer) REFERENCES TVolunteer(id),
+    CONSTRAINT fk_applicant FOREIGN KEY (id_applicant) REFERENCES TApplicant(id)
 );
