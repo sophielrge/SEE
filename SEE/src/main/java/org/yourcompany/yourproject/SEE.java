0e169816766bf6e8f760265ae1c56b7f0a43c0c1
sophielrge <sophie.large@wanadoo.fr>
2024-10-07 13:02:32 +0200

    SQL

--- SEE/src/main/java/org/yourcompany/yourproject/SEE.java
+++ SEE/src/main/java/org/yourcompany/yourproject/SEE.java
@@ -4,6 +4,11 @@
 
 package org.yourcompany.yourproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author sophie
@@ -16,7 +21,27 @@ public class SEE {
         COMPLETED 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+         // URL de connexion au serveur MySQL
+        String url = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/"; 
+        String user = "projet_gei_007"; 
+        String password = "deiD1ou2";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        conn = DriverManager.getConnection(url, user, password);
+        String sql = "INSERT INTO User (nom, email) VALUES (?,?)";
+        pstmt = conn.prepareStatement(sql);
+
+        // Paramètres à insérer
+        pstmt.setString(1, "Elise");  
+        pstmt.setString(2, "elise.gimond");
+
+        // Exécuter la requête
+        int rowsAffected = pstmt.executeUpdate();
+        System.out.println(rowsAffected + " ligne(s) insérée(s)");
+
         Applicant a = new Applicant("Sophax", 20);
         Request r = a.makeRequest();
         System.out.println("requete :" + r.getSubject() + " date: " + r.getDate());
